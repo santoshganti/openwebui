fe7e3c711c3f95341072c12026e6f6960f1ce97d
Santosh Ganti <santosh.ganti@live.com>
2025-06-04 17:05:26 -0700

    updating openwebui

--- searxng/searxng.sh
+++ searxng/searxng.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-sudo docker exec -it openwebui curl -k "https://searxng.truenas.darkcatalist.com/search?q=test&format=json"
+sudo docker exec -it openwebui curl -k "https://searxng.services.darkcatalist.com/search?q=test&format=json"
+
+sudo docker exec -it openwebui curl -k "http://openwebui-searxng:8080"
+
+sudo docker exec -it openwebui curl -k "http://10.0.0.20:30068"
