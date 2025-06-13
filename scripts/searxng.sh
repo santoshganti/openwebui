bc5460dee68ef272ee784ef40e97316dbb5d92fa
Santosh Ganti <santosh.ganti@live.com>
2025-06-12 23:41:53 -0700

    updating code

--- scripts/searxng.sh
+++ scripts/searxng.sh
@@ -2,5 +2,5 @@
 
 sudo docker exec -it openwebui curl -k "http://openwebui-searxng:8080/search?q=Google&format=json"
 sudo docker exec -it openwebui curl -k "http://openwebui-searxng:8080"
-
+sudo docker exec -it openwebui curl -k "http://openwebui-tika:9998"
 sudo docker exec -it openwebui curl -k "http://openwebui-playwright:3000"
